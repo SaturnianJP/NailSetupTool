62c626a83dbfed95fe979d1f4ab859818e0482b3
kurokiyuki <saturnianpersonal@gmail.com>
2024-04-14 01:45:29 +0900

    fixed for vrc

--- Packages/satania.tools.nailsetup/Editor/BoneParentTool.cs
+++ Packages/satania.tools.nailsetup/Editor/BoneParentTool.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 
@@ -129,4 +128,4 @@ namespace satania.shopping.tool
         #endregion
     }
 }
-
+#endif
--- Packages/satania.tools.nailsetup/Editor/NailSetupTool.cs
+++ Packages/satania.tools.nailsetup/Editor/NailSetupTool.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿#if UNITY_EDITOR
+using System.IO;
 using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
@@ -818,3 +819,4 @@ R_ThumbFinger
     }
 }
 
+#endif
